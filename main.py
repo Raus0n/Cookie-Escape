7e8ffbb9dcd8b4b795eb5975764b324c45343c44
testroyer <alperozer2007@gmail.com>
2022-08-22 17:40:35 +0300

    Tile movement

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ SCREEN_WIDTH = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH , SCREEN_HEIGHT))
 pygame.display.set_caption("Based Game")
 
-tile = Tile(64 , (111 , 111))
 tile_group = pygame.sprite.Group()
+tile = Tile(64 , (0 , 0))
 tile_group.add(tile)
+
 
 
 running = True
@@ -22,5 +23,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    key = pygame.key.get_pressed()
+    stripes = tile_group.sprites()
+    if key[pygame.K_d]:
+        for stripe in tile_group:
+            stripe.x_slide = 1
+    else:
+        for stripe in tile_group:
+            stripe.x_slide = 0
+
+    tile_group.update()
     tile_group.draw(screen)
     pygame.display.update()
--- tiles.py
+++ tiles.py
@@ -3,6 +3,10 @@ import pygame
 class Tile(pygame.sprite.Sprite):
     def __init__(self, size , pos) -> None:
         super().__init__()
+        self.x_slide = 0
         self.image = pygame.Surface((size ,size))
         self.image.fill((0 , 0 ,0))
         self.rect = self.image.get_rect(topleft = pos)
+
+    def update(self):
+        self.rect.x += self.x_slide
