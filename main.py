8291b1bdc9841973c4b3eaab903ddff8f026884d
Raus0n <rauson68@hotmail.com>
2022-09-01 22:13:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
 import pygame
 from level import Level
-import settings
+import settings,os
 
 pygame.init()
 
 SCREEN_HEIGHT = 960
 SCREEN_WIDTH = 960
 
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+FPS = 60
+
 screen = pygame.display.set_mode((SCREEN_WIDTH , SCREEN_HEIGHT))
 pygame.display.set_caption("Based Game")
 
 level = Level(settings.level_map , screen)
 
+def main():
+    running = True
+    while running:
 
-running = True
-while running:
-
-    screen.fill((255,255,255))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        screen.fill((255,255,255))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
 
 
     level.run()
