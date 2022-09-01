aa02c8beaea596326652873d7fec7b620ede98c7
Raus0n <rauson68@hotmail.com>
2022-09-01 22:27:25 +0300

    lol2

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ pygame.init()
 SCREEN_HEIGHT = 960
 SCREEN_WIDTH = 960
 
+TRACKMANIA_CAR = pygame.image.load("tm car.png")
+TRACKMANIA_CAR_IMAGE = pygame.transform.scale(TRACKMANIA_CAR, (105,80))
+TRACKMANIA_CAR_IMAGE = pygame.transform.rotate(TRACKMANIA_CAR_IMAGE, 40)
+
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 
@@ -18,17 +22,18 @@ pygame.display.set_caption("Based Game")
 level = Level(settings.level_map , screen)
 
 
-def draw_screen():
+def draw_screen(tmnf):
     screen.fill((255,255,255))
+    screen.blit(TRACKMANIA_CAR_IMAGE, (tmnf.x,tmnf.y))
     #şuraya çizilecek şeyleri koycaz işte, buraya level,resim vs koyacaksanız ayarlarsınız. bu arada resim loadlamak da çok kolaymış mesela örnek:
     pygame.display.update()
 def main():
-    
+    tmnf = pygame.Rect(100, 300, 105,80)
     clock = pygame.time.Clock()
     running = True
     while running:
         clock.tick(FPS)
-        draw_screen()
+        draw_screen(tmnf)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
